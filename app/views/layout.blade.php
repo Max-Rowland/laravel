c7aa1c4a6dbef1edc49d6f539dcc8187dbc29a96
Max-Rowland <m.rowland507@gmail.com>
2014-08-08 17:19:46 -0400

    added nav bar and logo

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -4,9 +4,18 @@
 		{{ HTML::script('js/jquery.min.js') }}
 		{{ HTML::script('js/jquery.uniform.min.js') }}
 		{{ HTML::script('js/jquery-ui.min.js') }}
+		{{ HTML::style('css/style.css'); }}
 	</head>
     <body>
-        <h1>Header and Nav Bar should go here</h1>
-        @yield('content')
+    	<div align="center">
+    		{{ HTML::image('images/sim-logo.png', 'alt', array( 'width' => 200, 'height' => 70 )) }}
+    		<div id="navbar">
+				<a href="depts">Departments</a>
+				<a href="emps">Employees</a>
+				<a href="titles">Job Titles</a>
+				<a href="admin/login">Login</a>
+			</div>
+	        @yield('content')
+    	</div>
     </body>
 </html>
